e26b6f76a8e27dbe53f712ada42f1d529ad6c35d
Andrew Vlietstra <vlietstra123@hotmail.com>
2022-05-07 12:39:54 -0400

    Played around with path and file system modules

--- node-tutorial-brad-traversy/index.js
+++ node-tutorial-brad-traversy/index.js
@@ -5,8 +5,3 @@ const andrew = new Person('Andrew', 26);
 console.log(andrew);
 andrew.greeting();
 
-function sayHello() {
-  return console.log(`Hey there ${this.name}`);
-};
-
-sayHello.call(andrew);
